b1578f7dbee481c7d7e210efd215fdc242ac7a36
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2020-03-02 16:18:12 +0100

    Added comment and code to make semantic tags functional in IE8 and bellow.

--- javascript/hvordan-affald-sorteres-i-odense-komune.js
+++ javascript/hvordan-affald-sorteres-i-odense-komune.js
@@ -1,2 +1,10 @@
 //Udvid sektion for affaldssortering ved tast//
 //Luk sektion for affaldssortering ved tryk på "X"//
+
+// ADD SEMANTIC FUNCTIONALITY TO IE 8 AND BELLOW
+document.createElement('nav'); //nav is semantic element
+document.createElement('header'); //header is semantic element
+document.createElement('main'); //main is semantic element
+document.createElement('section'); //section is semantic element
+document.createElement('article'); //article is semantic element
+document.createElement('footer'); //footer is semantic element
