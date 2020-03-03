57cc09227e157e7c1d6f571f0f5a223b31ddc18e
Lucas <60382112+Lukoly@users.noreply.github.com>
2020-03-03 09:08:35 +0100

    wer

--- javascript/hvordan-affald-sorteres-i-odense-komune.js
+++ javascript/hvordan-affald-sorteres-i-odense-komune.js
@@ -1,2 +1,12 @@
 //Udvid sektion for affaldssortering ved tast//
+//Indsæt linje med forklaring og eksempel//
 //Luk sektion for affaldssortering ved tryk på "X"//
+//
+//Peekaboo med link til sorteringsguide//
+// ADD SEMANTIC FUNCTIONALITY TO IE 8 AND BELLOW
+document.createElement('nav'); //nav is semantic element
+document.createElement('header'); //header is semantic element
+document.createElement('main'); //main is semantic element
+document.createElement('section'); //section is semantic element
+document.createElement('article'); //article is semantic element
+document.createElement('footer'); //footer is semantic element
