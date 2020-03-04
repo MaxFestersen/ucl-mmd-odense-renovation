3190b04e91f8d9e2e79e0e581a1b751cd3e81818
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2020-03-04 12:15:51 +0100

    Readded cookie code.

--- javascript/hvordan-affald-sorteres-i-odense-komune.js
+++ javascript/hvordan-affald-sorteres-i-odense-komune.js
@@ -24,30 +24,41 @@ document.createElement('footer'); //footer is semantic element
 
 /*Cookie code*/
 /*Code based on https://www.w3schools.com/js/js_cookies.asp*/
+var studieopgave_oplysning = document.getElementById("studieopgave_oplysning");
+var studieopgave_oplysning_closebth = document.getElementById("studieopgave_oplysning_closebth");
+
 function getCookie(){
-  var name = "view_setting" + "=";
-  var decodedCookie = decodeURIComponent(document.cookie);
-  var ca = decodedCookie.split(';');
-  for(var i = 0; i <ca.length; i++) {
-	var c = ca[i];
-	while (c.charAt(0) == ' ') {
-	  c = c.substring(1);
+	//console.log('Getting cookies')
+	var name = "studieopgave_oplysning" + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for(var i = 0; i <ca.length; i++) {
+		//console.log('Searching for cookie content')
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			//console.log('Validating cookie content')
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			//console.log('Found cookie match')
+			studieopgave_oplysning.style.display='none';
+			console.log("#studieopgave_oplysning has been hidden.");
+			return c.substring(name.length, c.length);
+		}
 	}
-	if (c.indexOf(name) == 0) {
-	 console.log("View settings found.");
-	 return c.substring(name.length, c.length);
-	}
-  }
-  console.log("No view settings found. Sorry boss.");
-  return "";
+	//console.log('Found matching cookie.')
+	return "";
+}
+getCookie();
+
+function hideAndSave() {
+	//console.log('Saving cookie.')
+	var d = new Date();
+	d.setTime(d.getTime() + (720*24*60*60*1000));
+	var expires = "expires="+ d.toUTCString();
+	document.cookie = "studieopgave_oplysning" + "=" + 1 + ";" + expires + ";path=/";
+	studieopgave_oplysning.style.display='none';
+	console.log('cookie is saved.')
 }
 
-function save_view_setting() {
-  var d = new Date();
-  d.setTime(d.getTime() + (720*24*60*60*1000));
-  var expires = "expires="+ d.toUTCString();
-  document.cookie = "view_setting" + "=" + view_setting + ";" + expires + ";path=/";
-  console.log("Saved view settings");
-  alert("Dine indstillinger er nu gemt.");
-
-}
+studieopgave_oplysning_closebth.onclick = hideAndSave;
