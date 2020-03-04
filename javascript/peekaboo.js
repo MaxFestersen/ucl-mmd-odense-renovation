7b833d44db95523b36c41b9c424ddd276cb12c36
Oeberg <60393289+Oeberg@users.noreply.github.com>
2020-03-04 12:42:29 +0100

    peekaboo virker

--- javascript/peekaboo.js
+++ javascript/peekaboo.js
@@ -1,15 +1,13 @@
 // peekaboo transition på scroll
-<
-script >
-    function yScrollHandler() {
-        var win = document.getElementById("peekaboo");
-        if ((window.pageYOffset + window.innerHeight) >= document.body.offsetHeight) {
-            //win.style.webkitTransition = "right 0.7s ease-in-out 0s";
-            win.style.transition = "right 0.7s ease-in-out 0s";
-            win.style.right = "0px";
-        } else {
-            win.style.transition = "right 0.7s ease-in-out 0s";
-            win.style.right = "-452px";
-        }
+function yScrollHandler() {
+    var win = document.getElementById("peekaboo");
+    if ((window.pageYOffset + window.innerHeight) >= document.body.offsetHeight) {
+        //win.style.webkitTransition = "right 0.7s ease-in-out 0s";
+        win.style.transition = "right 0.7s ease-in-out 0s";
+        win.style.left = "0px";
+    } else {
+        win.style.transition = "right 0.7s ease-in-out 0s";
+        win.style.left = "-345px";
     }
-window.onscroll = yScrollHandler; < /script>
+}
+window.onscroll = yScrollHandler;
