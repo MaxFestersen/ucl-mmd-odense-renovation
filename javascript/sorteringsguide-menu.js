461d4359434fa5340b8fbfe9ba615e8d998efe3d
Lucas <60382112+Lukoly@users.noreply.github.com>
2020-03-04 14:56:06 +0100

    added onclick function for top categories

---
+++ javascript/sorteringsguide-menu.js
@@ -0,0 +1,20 @@
+//Udvid sektion for affaldssortering ved tast
+var Madaffald_button = document.getElementById("madaffald-button");
+var top_display = document.getElementById("top-display");
+function fuck(color){
+  console.log("virker");
+  top_display.style.backgroundColor = color;
+  top_display.classList.remove("hidden");
+}
+var Restaffald_button = document.getElementById("restaffald-button");
+var Glas_button = document.getElementById("glas-button");
+var Glas_button = document.getElementById("metal-button");
+  //MADAFFALD onclick remove "display: hidden"
+  //MADAFFALD  onlick "close" add "display hidden"
+
+
+
+//Indsæt linje med forklaring og eksempel
+
+
+//Luk sektion for affaldssortering ved tryk på "X"
