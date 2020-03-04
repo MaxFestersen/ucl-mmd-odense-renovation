224d42d1da91fbbd545baa8c64aa10f3d0510678
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2020-03-04 10:48:23 +0100

    NOT WORKING: Added cookie content inspiration.

--- javascript/hvordan-affald-sorteres-i-odense-komune.js
+++ javascript/hvordan-affald-sorteres-i-odense-komune.js
@@ -17,3 +17,36 @@ document.createElement('main'); //main is semantic element
 document.createElement('section'); //section is semantic element
 document.createElement('article'); //article is semantic element
 document.createElement('footer'); //footer is semantic element
+
+
+/*Cookie code*/
+/*Code based on https://www.w3schools.com/js/js_cookies.asp*/
+function getCookie(){
+  var name = "view_setting" + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i <ca.length; i++) {
+	var c = ca[i];
+	while (c.charAt(0) == ' ') {
+	  c = c.substring(1);
+	}
+	if (c.indexOf(name) == 0) {
+	 console.log("View settings found.");
+	 return c.substring(name.length, c.length);
+	}
+  }
+  console.log("No view settings found. Sorry boss.");
+  return "";
+}
+
+function save_view_setting() {
+  var d = new Date();
+  d.setTime(d.getTime() + (720*24*60*60*1000));
+  var expires = "expires="+ d.toUTCString();
+  document.cookie = "view_setting" + "=" + view_setting + ";" + expires + ";path=/";
+  console.log("Saved view settings");
+  alert("Dine indstillinger er nu gemt.");
+
+}
+
+
