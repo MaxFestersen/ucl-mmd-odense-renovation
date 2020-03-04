42a951640d135508f436f91f7a0ee64c06047bc1
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2020-03-04 10:46:21 +0100

    Added empty lines.

--- javascript/hvordan-affald-sorteres-i-odense-komune.js
+++ javascript/hvordan-affald-sorteres-i-odense-komune.js
@@ -1,7 +1,14 @@
 //Udvid sektion for affaldssortering ved tast//
+
+
 //Indsæt linje med forklaring og eksempel//
+
+
 //Luk sektion for affaldssortering ved tryk på "X"//
+
+
 //Peekaboo med link til sorteringsguide//
+
 
 // ADD SEMANTIC FUNCTIONALITY TO IE 8 AND BELLOW
 document.createElement('nav'); //nav is semantic element
