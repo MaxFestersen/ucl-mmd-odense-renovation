d220d70d42b521cc83e9575c79065cab05a0cda0
MaxFestersen <49905988+MaxFestersen@users.noreply.github.com>
2020-03-05 18:38:25 +0100

    Opdateret funktionensnavnet fuck til showDisplay. Refferencer opdateret. Opdateret funktionen showDisplay. Refferencer opdateret. Tilføjet id til links som resultat. Flyttet rundt på JS indhold. Fjernet eller udkommenteret console.log. Opdateret JS beskrivelser. Fjerenet alle static_ refferencer i alle dokumenter, og erstattet med main_. Tilføjet alt og title til statiske ikoner. Flere mindre CSS ændringer.

--- javascript/sorteringsguide-menu.js
+++ javascript/sorteringsguide-menu.js
@@ -1,26 +1,104 @@
+// INTERACTIVE MAIN CONTENT JavaScript
+
+// VARS
+var MadaffaldButton = document.getElementById("madaffald-button");
+var topDisplay = document.getElementById("top-display");
+var bottomDisplay = document.getElementById("bottom-display");
+var restaffaldButton = document.getElementById("restaffald-button");
+var glasButton = document.getElementById("glas-button");
+var metalButton = document.getElementById("metal-button");
+
 //Udvid sektion for affaldssortering ved tast
-var Madaffald_button = document.getElementById("madaffald-button");
-var top_display = document.getElementById("top-display");
-function fuck(color){
-  console.log("virker");
-  top_display.style.backgroundColor = color;
-  top_display.classList.remove("hidden");
+function showDisplay(color, id){
+  switch(id) {
+	case 'madaffald-button':
+	  // vars
+	  var category = document.getElementById("madaffald");
+	  var display = topDisplay;
+	  break;
+	case 'restaffald-button':
+	  // vars
+	  var category = document.getElementById("restaffald");
+	  var display = topDisplay;
+	  break;
+	case 'glas-button':
+	  // vars
+	  var category = document.getElementById("glas");
+	  var display = topDisplay;
+	  break;
+	case 'metal-button':
+	  // vars
+	  var category = document.getElementById("metal");
+	  var display = topDisplay;
+	  break;
+	case 'papir-button':
+	  // vars
+	  var category = document.getElementById("papir");
+	  var display = bottomDisplay;
+	  break;
+	case 'smaat-pap-button':
+	  // vars
+	  var category = document.getElementById("smaat-pap");
+	  var display = bottomDisplay;
+	  break;
+	case 'plast-button':
+	  // vars
+	  var category = document.getElementById("plast");
+	  var display = bottomDisplay;
+	  break;
+	case 'oevrigt-button':
+	  // vars
+	  var category = document.getElementById("oevrigt");
+	  var display = bottomDisplay;
+	  break;
+	default:
+	  console.log('Kategorien blev ikke genkendt.')
+	  return(false);
+	}
+  // Update logo
+  var staticIcon = getFirstClassFromId(category, 'main_content_icon');
+  var icon = getFirstClassFromId(display, 'main_content_icon');
+  icon.src = staticIcon.src; // Opdaterer src (billederefferencen)
+  icon.alt = staticIcon.alt; // Opdaterer alt (teksten der står hvis billedet ikke bliver indlæst)
+  icon.title = staticIcon.title; // Opdaterer title (tooltip)
+
+  // Update title
+  // hentested = definer hvor det skal hentes
+  // placering = definer hvor skal sættes
+  // sæt placering = hentested
+
+  // Update content
+  var staticContent = getFirstClassFromId(category, 'main_content_content');
+  var topContent = getFirstClassFromId(display, 'main_content_content');
+  topContent.innerHTML = staticContent.innerHTML;
+  
+  // Show section
+  //console.log("#top-display visable by removal of hidden class.");
+  display.style.backgroundColor = color;
+  display.classList.remove("hidden");
+  
+  // Hide section
+  if(display == bottomDisplay){
+	  hideTopdisplay()
+  } else {
+	  // hidebottomdisplay?
+  }
 }
-var Restaffald_button = document.getElementById("restaffald-button");
-var Glas_button = document.getElementById("glas-button");
-var Glas_button = document.getElementById("metal-button");
-  //MADAFFALD onclick remove "display: hidden"
-  //MADAFFALD  onlick "close" add "display hidden"
+
+function getFirstClassFromId(id, Class) {
+  var content = id.getElementsByClassName(Class);
+  content = content[0];
+  return(content)
+}
+//MADAFFALD  onlick "close" add "display hidden"
 function hideTopdisplay(){
-  top_display.classList.add("hidden");
+  console.log("#top-display hidden with hidden class.")
+  topDisplay.classList.add("hidden");
 }
-//On esc press//
-console.log("test")
+//On esc press
 document.addEventListener("keyup", event=> {
   const key = event.key.toLowerCase();
-  console.log("prøve")
   if(key=="escape"){
-    console.log("hideTopdisplay")
     hideTopdisplay();
   }
 });
